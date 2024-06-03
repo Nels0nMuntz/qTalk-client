7160cc043aaca9b64233e91fd57177b99cbf4bf2
Eugene Chekerdes <tchekerdes.vgen@ukr.net>
2024-06-03 07:51:30 +0300

    fixed navbar height

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export default async function Navbar() {
   const session = await getServerSession(authoOptions);
 
   return (
-    <header className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
+    <header className="fixed top-0 inset-x-0 h-auto bg-zinc-100 border-b border-zinc-300 z-10 py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="w-8 h-8 sm:w-6 sm:h-6" />
